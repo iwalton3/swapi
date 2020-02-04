5407dd51a3a0dd40519f29e16b5548ce426e2f1d
Ian Walton <ian@iwalton.com>
2020-02-04 13:57:03 -0500

    Add setup.py.

---
+++ server/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+setup(
+    name='swapi',
+    version='1.0.0',
+    author="Ian Walton",
+    author_email="iwalton3@gmail.com",
+    description="New template for making applications on iwalton.com.",
+    license='LGPLv3',
+    url="https://github.com/iwalton3/swapi",
+    py_modules=['swa', 'email_session_manager', 'swa_gen_py', 'swa_gen_js'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pymysql', 'sqlalchemy', 'werkzeug']
+)
+
